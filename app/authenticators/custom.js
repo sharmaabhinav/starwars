975f97fb430b9e245a040cde192d3d009885aacd
Abhinav Sharma <abhinav.sharma@jifflenow.com>
2017-06-02 14:28:34 +0530

    Fixed undefined userinfo issue in autheticator

--- app/authenticators/custom.js
+++ app/authenticators/custom.js
@@ -8,7 +8,7 @@ export default Base.extend({
     var promise = new Promise( (resolve, reject) => {
       this.get('swapi').getPassword(username).then((response) => {
         let userInfo = response.results[0]
-        if(userInfo.birth_year === password && userInfo.name === username){
+        if(userInfo && userInfo.birth_year === password && userInfo.name === username){
           resolve('Correct username and password')
         }else{
           reject('Incorrect username /  password')
